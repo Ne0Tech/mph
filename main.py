99b268af4971c07eaf718874c309516c0fbf3ced
Zavian Pedigo <pedigzav001@warren.k12.in.us>
2024-08-28 09:56:16 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+miles = 5 #pretend this came from user input
+minutes = 45 #pretend this came from user input
+hours = minutes / 60
+
+mph = miles / hours
+
+print("How many miles did you run?", miles)
+print("How many minutes did it take?", minutes)
+print("Speed in mph" , mph)
